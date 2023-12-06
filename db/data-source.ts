ee8ef49c9b1bc221e7024d456564a650e7957432
SeyedMostafaHosseinian <seyedmostafainsta786@gmail.com>
2023-12-06 13:46:01 +0330

    install and config typeorm and add env

---
+++ db/data-source.ts
@@ -0,0 +1,18 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const ormconfig: DataSourceOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  password: process.env?.DB_PASSWORD,
+  username: process.env?.DB_USERNAME,
+  database: process.env?.DB_NAME,
+  entities: ['dist/**/*.entity.js'],
+  migrations:['dist/db/migrations/*.js']
+};
+ 
+const dataSource = new DataSource(ormconfig);
+export default dataSource;
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TagsModule } from './tags/tags.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormconfig } from '../db/data-source';
 
 @Module({
-  imports: [TagsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(ormconfig),
+    TagsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
