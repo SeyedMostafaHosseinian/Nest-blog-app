ff84c24e7505be3afeea6bdf844473a49c20f1d8
SeyedMostafaHosseinian <seyedmostafainsta786@gmail.com>
2023-12-06 14:20:28 +0330

    add  tags entity

---
+++ db/migrations/1701859596538-migration.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Migration1701859596538 implements MigrationInterface {
+    name = 'Migration1701859596538'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "tags" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, CONSTRAINT "PK_e7dc17249a1148a1970748eda99" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "tags"`);
+    }
+
+}
--- src/tags/entities/tag.entity.ts
+++ src/tags/entities/tag.entity.ts
@@ -1,1 +1,10 @@
-export class Tag {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'tags' })
+export class TagEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('varchar')
+  name: string;
+}
