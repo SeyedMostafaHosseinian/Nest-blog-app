8b860bd4364e7c4e9b1ba79620679e92a1db549f
SeyedMostafaHosseinian <seyedmostafainsta786@gmail.com>
2023-12-20 13:28:10 +0330

    enable cors in backend app and correct start script

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  });
   await app.listen(3000);
 }
 bootstrap();
